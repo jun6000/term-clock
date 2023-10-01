1e000a3687d78320ce594abfbb0ce161b04f853f
Arjun Vishanth <arjunvishanth@gmail.com>
2023-10-01 11:28:29 +0530

    Clock working now.
    TODO
    add q to stop.
    Add 12hr format
    Add some sort of blinking effect / animation

--- index.js
+++ index.js
@@ -1,16 +1,46 @@
 #!/usr/bin/env node
 
-function printDigits(inputs) {
-    result = "\n\n";
+function printDigits(args) {
+    let result = "\n\n";
     for (let j = 0; j < 6; j++) {
-        for (let i = 0; i < inputs.length; i++) {
-            result += digits[inputs[i]][j] + " ";
+        for (let i = 0; i < args.length; i++) {
+            result += digits[args[i]][j] + " ";
         };
         result += "\n";
     };
 
     console.log(result);
 };
+
+function printTime(args) {
+    const [ hours, minutes, seconds ] = args;
+    let result = "\n";
+    for (let i = 0; i < 6; i++) {
+        result += digits[Math.floor(hours / 10)][i] + ' ' + digits[hours % 10][i] + ' ' + colon[i] + ' '
+                + digits[Math.floor(minutes / 10)][i] + ' ' + digits[minutes % 10][i] + ' ' + colon[i] + ' '
+                + digits[Math.floor(seconds / 10)][i] + ' ' + digits[seconds % 10][i] + '\n';
+    };
+
+    console.log(result);
+};
+
+function getTime(format) {
+    const now = new Date();
+    const hours = now.getHours();
+    const minutes = now.getMinutes();
+    const seconds = now.getSeconds();
+
+    return [ hours, minutes, seconds ];
+};
+
+const colon = [
+    "   ",
+    "██╗",
+    "╚═╝",
+    "██╗",
+    "╚═╝",
+    "   "
+];
 
 const digits = [
     [
@@ -95,5 +125,7 @@ const digits = [
     ]
 ];
 
-// console.log(new Date().toLocaleTimeString("en-US", { hour12: true }));
-printDigits([1, 2, 3]);
+setInterval(() => {
+    console.clear();
+    printTime(getTime(0));
+}, 1000);
