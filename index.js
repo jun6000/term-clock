7be0d5257cf6763fc6fc22f9566e6f57e0d7d93c
Arjun Vishanth <arjunvishanth@gmail.com>
2023-10-02 10:51:17 +0530

    Centered text horizontally.
    Might add 12/24hr format and option to hide seconds later.

--- index.js
+++ index.js
@@ -5,20 +5,23 @@ import { createRequire } from "module";
 const require = createRequire(import.meta.url);
 const hideCursor = require("hide-terminal-cursor");
 hideCursor();
+const center = require("center-align");
 
 function printTime(args) {
     const [ hours, minutes, seconds ] = args;
     let result = "\n";
+
     for (let i = 0; i < 6; i++) {
-        result += digits[Math.floor(hours / 10)][i] + ' ' + digits[hours % 10][i] + ' ' + colon[i] + ' '
+        let tmp = digits[Math.floor(hours / 10)][i] + ' ' + digits[hours % 10][i] + ' ' + colon[i] + ' '
                 + digits[Math.floor(minutes / 10)][i] + ' ' + digits[minutes % 10][i] + ' ' + colon[i] + ' '
-                + digits[Math.floor(seconds / 10)][i] + ' ' + digits[seconds % 10][i] + '\n';
+                + digits[Math.floor(seconds / 10)][i] + ' ' + digits[seconds % 10][i];
+        result += center(tmp, process.stdout.columns) + '\n';
     };
 
     chalkAnimation.pulse(result, 0.9);
 };
 
-function getTime(format) {
+function getTime() {
     const now = new Date();
     const hours = now.getHours();
     const minutes = now.getMinutes();
@@ -121,5 +124,5 @@ const digits = [
 
 setInterval(() => {
     console.clear();
-    printTime(getTime(0));
+    printTime(getTime());
 }, 1000)
